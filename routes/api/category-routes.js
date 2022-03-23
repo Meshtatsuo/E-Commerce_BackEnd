60925300aea5ec44f49278a2af2ec817116c2dcd
meshtatsuo <meshtatsuo@gmail.com>
2022-03-23 15:49:48 -0600

    getting some routes understood

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,35 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const {
+  Category,
+  Product
+} = require('../../models');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  res.send(`<h1>GET ALL CATEGORIES</h1>`);
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  res.send(`<h1>GET SINGLE CATEGORY</h1>`);
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  res.send(`<h1>POST NEW CATEGORY</H1>`);
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  res.send(`<h1>UPDATE CATEGORY BY ID</H1>`);
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  res.send(`<h1>DELETE CATEGORY BY ID</H1>`);
 });
 
 module.exports = router;
