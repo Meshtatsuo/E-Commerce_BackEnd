eb5a6b128362bdc1d442aeaa1e6b19b72fa2562c
meshtatsuo <meshtatsuo@gmail.com>
2022-03-25 17:47:50 -0600

    tag-routes completed

--- models/Tag.js
+++ models/Tag.js
@@ -10,12 +10,13 @@ class Tag extends Model {}
 Tag.init({
   id: {
     type: DataTypes.INTEGER,
-    primaryKey: true,
     allowNull: false,
-    autoIncrement: true,
+    primaryKey: true,
+    autoIncrement: true
   },
   tag_name: {
-    type: DataTypes.STRING
+    type: DataTypes.STRING,
+    allowNull: false,
   }
 }, {
   sequelize,
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const path = require('path');
 const apiRoutes = require('./api');
+const tag_routes = require('./api/tag-routes');
 
 router.use('/api', apiRoutes);
+router.use('/api/tags', tag_routes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
